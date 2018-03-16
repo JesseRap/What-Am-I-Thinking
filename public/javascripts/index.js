0e7f04c503a8933371736533dd4b059ca4cab843
Jesse Rappaport <jdr@Jesses-MBP.home>
2018-03-16 18:49:46 -0400

    implement vue findGame button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,14 @@
+// Set up socket
 var socket = io();
 console.log(socket);
 
+function User(id) {
+  this.id = id;
+}
+
+const findGameBtn = document.querySelector('.findGameBtn');
+const readyToPlayBtn = document.querySelector('.readyToPlayBtn');
+findGameBtn.dataset.state = 'initial';
 
 socket.on('user table changed', (data) => {
   console.log("USER CHANGE", data);
@@ -8,24 +16,78 @@ socket.on('user table changed', (data) => {
   users.userCount = data.userCount;
 })
 
-const findGameBtn = document.querySelector('.findGameBtn');
-const readyToPlayBtn = document.querySelector('.readyToPlayBtn');
 
-findGameBtn.addEventListener('click', (event) => {
-  console.log(event);
+const findGameClickHandler = function() {
   socket.emit('find game');
+  findGameBtn.innerHTML = 'WAITING FOR PARTNER';
+  findGameBtn.classList.remove('siimple-btn--green');
+  findGameBtn.classList.add('siimple-btn--red');
+  findGameBtn.removeEventListener('click', findGameClickHandler);
+  findGameBtn.addEventListener('click', unclickHandler);
+}
+const findGameMouseoverHandler = function() {
+  findGameBtn.innerHTML = 'CANCEL';
+}
+
+const findGameMouseoutHandler = function() {
+  findGameBtn.innerHTML = 'WAITING FOR PARTNER';
+}
+
+const unclickHandler = function() {
+  console.log("HERE");
+  findGameBtn.innerHTML = 'FIND GAME';
+  findGameBtn.removeEventListener('mouseover', findGameMouseoverHandler);
+  findGameBtn.removeEventListener('mouseout', findGameMouseoutHandler);
+  findGameBtn.removeEventListener('click', unclickHandler);
+  findGameBtn.addEventListener('click', findGameClickHandler);
+  findGameBtn.classList.add('siimple-btn--green');
+  findGameBtn.classList.remove('siimple-btn--red');
+}
+
+findGameBtn.addEventListener('click', findGameClickHandler);
+
+
+findGameBtn.addEventListener('click', click = function(event) {
+  console.log(event);
+  if (event.target.dataset.state === 'initial') {
+    socket.emit('find game');
+    findGameBtn.innerHTML = 'WAITING FOR PARTNER';
+    findGameBtn.classList.remove('siimple-btn--green');
+    findGameBtn.classList.add('siimple-btn--red');
+
+    findGameBtn.addEventListener('mouseover', mouseover = function(event) {
+      findGameBtn.innerHTML = "CANCEL"
+    });
+    findGameBtn.addEventListener('mouseout', mouseout = function(event) {
+      findGameBtn.innerHTML = "WAITING FOR PARTNER"
+    });
+    findGameBtn.addEventListener('click', unclick = function(event) {
+      findGameBtn.innerHTML = 'FIND GAME';
+      findGameBtn.removeEventListener('mouseover', mouseover);
+      findGameBtn.removeEventListener('mouseout', mouseout);
+      findGameBtn.removeEventListener('click', unclick);
+      findGameBtn.addEventListener('click', click);
+      findGameBtn.dataset.state = 'initial';
+      findGameBtn.classList.add('siimple-btn--green');
+      findGameBtn.classList.remove('siimple-btn--red');
+    });
+
+  }
+
+
+
 });
 
 socket.on('start new room', (data) => {
-  console.log(data);
+  console.log(data, findGameBtn);
   findGameBtn.disabled = true;
-  readyToPlayBtn.disabled = false;
+  // readyToPlayBtn.disabled = false;
 })
 
-readyToPlayBtn.addEventListener('click', (event) => {
-  console.log(event);
-  socket.emit('ready to play');
-});
+// readyToPlayBtn.addEventListener('click', (event) => {
+//   console.log(event);
+//   socket.emit('ready to play');
+// });
 
 socket.on('start game', (data) => {
   console.log("LET'S PLAY!!!");
--- public/javascripts/vue-components.js
+++ public/javascripts/vue-components.js
@@ -1,8 +1,19 @@
 // USERS table
 const users = new Vue({
-  el: '#users',
+  el: '#root',
   data: {
     usersOnline: [],
-    userCount: 0
+    userCount: 0,
+    findGameBtnStateInitial: true,
+
+    findGameHandler: function() {
+      socket.emit('find game');
+      users.findGameBtnStateInitial = false;
+    },
+
+    cancelFindGame: function() {
+      // EMIT MESSAGE TO SOCKET??
+      users.findGameBtnStateInitial = true;
+    }
   }
 });
